6747c7cc634ad733dbf1d0de6aae6d4130afa7f7
syafi-M <syafimq00@gmail.com>
2024-03-13 09:15:55 +0700

    UPDATED: VIEW ADMIN PANEL, ORDER COMPONENT

--- resources/js/Components/Order/OrderList/OrderListComponent.jsx
+++ resources/js/Components/Order/OrderList/OrderListComponent.jsx
@@ -1,97 +1,132 @@
-import { FormatRupiah } from "@arismun/format-rupiah"
-import dayjs from "dayjs"
+import Modal from "@/Components/Modal";
+import { FormatRupiah } from "@arismun/format-rupiah";
+import dayjs from "dayjs";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { FaRegFaceSadTear } from "react-icons/fa6";
 
 export default function OrderListComponent({ order }) {
-    return (
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  return (
     <>
-        {order.data.length  != 0?listData({ order }):noData()}
+      {order.data.length != 0 ? (
+        <ListData order={order} toggleModal={toggleModal} />
+      ) : (
+        <NoData />
+      )}
+      {modalOpen && <OrderModal show={modalOpen} toggleModal={toggleModal} />}
     </>
-  )
+  );
 }
 
+const OrderModal = ({ show, toggleModal }) => {
+  return (
+    <>
+      {show && (
+        <div className="fixed inset-0 bg-slate-400/70 transition-all ">
+          <AnimatePresence>
+            <motion.div
+              key="modal"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              transition={{ duration: 0.5 }}
+            >
+              <div className="">
+                Aku Modal
+                <button onClick={() => toggleModal()}>X</button>
+              </div>
+            </motion.div>
+          </AnimatePresence>
+        </div>
+      )}
+    </>
+  );
+};
 
-const listData = ({ order }) => {
-    return (
-    <table className="table table-xs table-zebra capitalize">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Client</th>
-                    <th>Items name</th>
-                    <th>Price</th>
-                    <th>Amount</th>
-                    <th>Piece</th>
-                    <th>Total</th>
-                    <th>Status</th>
-                    <th>Date</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-            {order.data.map((ord, i) => (
-                <tr key={i}>
-                    <td>{i + 1}</td>
-                    <td>{ord.client? ord.client.name : "~ Empty ~"}</td>
-                    <td>{ord.stock_stuff ? ord.stock_stuff.nama_barang : "~ Empty ~"}</td>
-                    <td>
-                        {ord.stock_stuff &&
-                            <FormatRupiah value={ord.stock_stuff.harga_satuan} />
-                        }
-                    </td>
-                    <td>{ord.jumlah}</td>
-                    <td>{ord.piece ? ord.piece.nama_satuan : "~ Empty ~"}</td>
-                    <td>
-                        {ord.stock_stuff &&
-                            <FormatRupiah
-                                value={
-                                    parseInt(ord.jumlah) *
-                                    parseInt(
-                                        ord.stock_stuff.harga_satuan
-                                    )
-                                }
-                            />
-                        }
-                    </td>
-                    <td>
-                        <span
-                            className={
-                                ord.approve == "waiting"
-                                    ? "bg-yellow-500 p-1 rounded-sm text-white font-semibold text-center"
-                                    : "bg-green-500 p-1 rounded-sm text-white font-semibold text-center"
-                            }
-                        >
-                            {ord.approve}
-                        </span>
-                    </td>
-                    <td>
-                        {dayjs(ord.tanggal)
-                            .locale("id")
-                            .format("DD MMMM YYYY")}
-                    </td>
-                    <td>
-                        <button className="btn btn-xs btn-info text-white">
-                            Open Order
-                        </button>
-                    </td>
-                </tr>
-            ))}
-            </tbody>
+const ListData = ({ order, toggleModal }) => {
+  return (
+    <table className="table table-xs table-zebra capitalize bg-white rounded-md">
+      <thead>
+        <tr>
+          <th className="bg-sky-300 rounded-tl-md">#</th>
+          <th className="bg-sky-300">Client</th>
+          <th className="bg-sky-300">Items name</th>
+          <th className="bg-sky-300">Price</th>
+          <th className="bg-sky-300">Amount</th>
+          <th className="bg-sky-300">Piece</th>
+          <th className="bg-sky-300">Total</th>
+          <th className="bg-sky-300">Status</th>
+          <th className="bg-sky-300">Date</th>
+          <th className="bg-sky-300 rounded-tr-md">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {order.data.map((ord, i) => (
+          <tr key={i}>
+            <td>{i + 1}</td>
+            <td>{ord.client ? ord.client.name : "~ Empty ~"}</td>
+            <td>
+              {ord.stock_stuff ? ord.stock_stuff.nama_barang : "~ Empty ~"}
+            </td>
+            <td>
+              {ord.stock_stuff && (
+                <FormatRupiah value={ord.stock_stuff.harga_satuan} />
+              )}
+            </td>
+            <td>{ord.jumlah}</td>
+            <td>{ord.piece ? ord.piece.nama_satuan : "~ Empty ~"}</td>
+            <td>
+              {ord.stock_stuff && (
+                <FormatRupiah
+                  value={
+                    parseInt(ord.jumlah) *
+                    parseInt(ord.stock_stuff.harga_satuan)
+                  }
+                />
+              )}
+            </td>
+            <td>
+              <span
+                className={
+                  ord.approve == "waiting"
+                    ? "bg-yellow-500 p-1 rounded-sm text-white font-semibold text-center"
+                    : "bg-green-500 p-1 rounded-sm text-white font-semibold text-center"
+                }
+              >
+                {ord.approve}
+              </span>
+            </td>
+            <td>{dayjs(ord.tanggal).locale("id").format("DD MMMM YYYY")}</td>
+            <td>
+              <button
+                onClick={() => toggleModal()}
+                className="btn btn-xs btn-info text-white"
+              >
+                Open Order
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
-    )
-}
+  );
+};
 
-const noData = () => {
-    return (
-        <>
-            <div className="text-center my-2 bg-white p-10 rounded-md shadow-sm">
-                <span className="text-center text-gray-400 font-bold flex flex-col gap-y-2 justify-center items-center">
-                    <FaRegFaceSadTear className="text-2xl"/>
-                    <span className="text-sm italic">
-                        - Data Not Available -
-                    </span>
-                </span>
-            </div>
-        </>
-    );
-}
+const NoData = () => {
+  return (
+    <>
+      <div className="text-center my-2 bg-white p-10 rounded-md shadow-sm">
+        <span className="text-center text-gray-400 font-bold flex flex-col gap-y-2 justify-center items-center">
+          <FaRegFaceSadTear className="text-2xl" />
+          <span className="text-sm italic">- Data Not Available -</span>
+        </span>
+      </div>
+    </>
+  );
+};
--- resources/js/Pages/Admin/Index.jsx
+++ resources/js/Pages/Admin/Index.jsx
@@ -2,61 +2,54 @@ import OrderIndex from "@/Components/Order/OrderIndex";
 import OrderCreate from "@/Components/Order/OrderCreate";
 import { useState } from "react";
 import { PiPackageDuotone } from "react-icons/pi";
-import {  RiBookReadLine } from "react-icons/ri";
+import { RiBookReadLine } from "react-icons/ri";
 import { Head } from "@inertiajs/react";
 import NavbarComponent from "@/Components/NavbarComponent";
 
 export default function Index({ user, order }) {
-    const [component, setComponent] = useState("admin");
+  const [component, setComponent] = useState("admin");
 
-    let isLink;
-    if (component === "orderI") {
-        isLink = <OrderIndex order={order} />;
-    } else if (component === "orderC") {
-        isLink = <OrderCreate />;
-    }
-    // console.log(usePage().props.ziggy.location);
-    return (
-        <>
-            <Head title="ADMIN PANEL"/>
-            <div class="min-h-screen bg-sky-100">
-                <NavbarComponent auth={user}/>
-                <div className="w-full h-[90vh] flex gap-4">
-                    <div className="w-1/4 m-5 p-2 drop-shadow-md bg-sky-500 rounded-lg ">
-                        <span>
-                            <p className="text-center text-white font-semibold">Menu Admin</p>
-                            <hr />
-                            <ul className="menu">
-                                <li>
-                                    <details>
-                                        <summary className="w-full">
-                                            <span className="flex items-center gap-2 text-base font-medium">
-                                                <PiPackageDuotone />{" "}
-                                                <p>Order</p>
-                                            </span>
-                                        </summary>
-                                        <ul>
-                                            <li>
-                                                <span className="flex gap-2">
-                                                    <RiBookReadLine />
-                                                    <button
-                                                        onClick={() =>
-                                                            setComponent("orderI")
-                                                        }
-                                                    >
-                                                        Data Orders
-                                                    </button>
-                                                </span>
-                                            </li>
-                                        </ul>
-                                    </details>
-                                </li>
-                            </ul>
+  let isLink;
+  if (component === "orderI") {
+    isLink = <OrderIndex order={order} />;
+  } else if (component === "orderC") {
+    isLink = <OrderCreate />;
+  }
+  // console.log(usePage().props.ziggy.location);
+  return (
+    <>
+      <Head title="ADMIN PANEL" />
+      <div class="min-h-screen bg-sky-100">
+        <NavbarComponent auth={user} />
+        <div className="w-full h-[89vh] flex gap-4">
+          <div className="w-1/4 m-5 p-2 drop-shadow-md bg-sky-500 rounded-lg ">
+            <span>
+              <p className="text-center text-white font-semibold">Menu Admin</p>
+              <hr />
+              <ul className="menu">
+                <li>
+                  <details>
+                    <summary className="w-full">
+                      <span className="flex items-center gap-2 text-base font-medium">
+                        <PiPackageDuotone /> <p>Order</p>
+                      </span>
+                    </summary>
+                    <ul>
+                      <li onClick={() => setComponent("orderI")}>
+                        <span className="flex gap-2">
+                          <RiBookReadLine />
+                          <button>Data Orders</button>
                         </span>
-                    </div>
-                    <div className="w-3/4 m-5">{isLink}</div>
-                </div>
-            </div>
-        </>
-    );
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </span>
+          </div>
+          <div className="w-3/4 m-5">{isLink}</div>
+        </div>
+      </div>
+    </>
+  );
 }
